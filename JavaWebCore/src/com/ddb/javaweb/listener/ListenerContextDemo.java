212a2454f1905d60e5d5a0186f9ca50428188234
dbdu <1797890817@qq.com>
2017-01-06 16:28:02 +0800

    fixed javaPrj_1 bug

---
+++ JavaWebCore/src/com/ddb/javaweb/listener/ListenerContextDemo.java
@@ -0,0 +1,58 @@
+package com.ddb.javaweb.listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ListenerContextDemo
+ *
+ */
+@WebListener
+public class ListenerContextDemo implements ServletContextListener, ServletContextAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ListenerContextDemo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent paramServletContextAttributeEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent paramServletContextAttributeEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent paramServletContextEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent paramServletContextAttributeEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent paramServletContextEvent)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
---
+++ JavaWebCore/src/com/ddb/javaweb/listener/ListenerSessionDemo.java
@@ -0,0 +1,96 @@
+package com.ddb.javaweb.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionIdListener;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class ListenerSessionDemo
+ *
+ */
+@WebListener
+public class ListenerSessionDemo implements HttpSessionListener, HttpSessionAttributeListener, HttpSessionBindingListener, HttpSessionActivationListener, HttpSessionIdListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ListenerSessionDemo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent paramHttpSessionEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueBound(HttpSessionBindingEvent)
+     */
+    public void valueBound(HttpSessionBindingEvent paramHttpSessionBindingEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionIdListener#sessionIdChanged(HttpSessionEvent, String)
+     */
+    public void sessionIdChanged(HttpSessionEvent paramHttpSessionEvent, String paramString)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent paramHttpSessionEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionDidActivate(HttpSessionEvent)
+     */
+    public void sessionDidActivate(HttpSessionEvent paramHttpSessionEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent paramHttpSessionBindingEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent paramHttpSessionBindingEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent paramHttpSessionBindingEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionWillPassivate(HttpSessionEvent)
+     */
+    public void sessionWillPassivate(HttpSessionEvent paramHttpSessionEvent)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueUnbound(HttpSessionBindingEvent)
+     */
+    public void valueUnbound(HttpSessionBindingEvent paramHttpSessionBindingEvent)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
